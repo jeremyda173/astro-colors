f89327ff48e4658ee6d4e561e8f68cb247500fd6
jeremyda173 <jeremygithud173@gmail.com>
2026-02-14 23:02:48 -0400

    feat: add astro-icon integration and cleanup config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
+  integrations: [icon()],
   devToolbar: {
     enabled: false
   }
